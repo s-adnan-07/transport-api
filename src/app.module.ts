ec392583b603976ce006fbdbc6f9ffa9b9648b9e
Adnan <s.adnan@aabtools.com>
2024-05-10 17:11:11 +0400

    Setup fastify platform

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), VehiclesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+
+type Environment = 'development' | 'production'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+  const configService = app.get(ConfigService)
+  const NODE_ENV = configService.get<Environment>('NODE_ENV')
+  const PORT =
+    NODE_ENV == 'development' ? 3000 : configService.get<number>('PORT')
+
+  await app.listen(PORT)
 }
 
 bootstrap()
