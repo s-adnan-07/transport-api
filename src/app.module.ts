120cf9880bd19fb11c67686797d7b059641267ac
Adnan <s.adnan@aabtools.com>
2024-05-11 10:34:23 +0400

    Added vehicle and operation Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { VehiclesModule } from './vehicles/vehicles.module';
+import { VehiclesModule } from './vehicles/vehicles.module'
+import { OperationsModule } from './operations/operations.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), VehiclesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    VehiclesModule,
+    OperationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
